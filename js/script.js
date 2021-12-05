1fe35fb7fb7b560d3f87239e8c78845449e2e1c1
Kaitlyn Joy Ingersoll <kaitlynjoyingersoll@gmail.com>
2021-12-04 18:05:56 -0600

    global variables

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const guessInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingMessage = document.querySelector(".remaining");
+const remaining = document.querySelector(".remaining span");
+const guessMessage = document.querySelector(".message");
+const playAgainButton = document.querySelector(".play-again");
+const word = "magnolia";
